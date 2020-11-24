aa91ba4a8837c9dc3362b601383a5564b80f43da
Mykyta Batrak <fighter662@gmail.com>
2020-11-24 14:20:25 +0200

    html:   Added IDs to all card elements. css:   Added classes: 'card-right' and 'card-wrong'. js:   card: added methods to rotate and change card color   game: made game mechanics work

--- source/js/card.js
+++ source/js/card.js
@@ -1,13 +1,49 @@
 class Card {
-  constructor(HTMLCardElement, face) {
+  constructor(HTMLCardElement) {
     this.card = HTMLCardElement;
-    this.card.children[0].children[1].innerHTML = face;
-    this.face = face;
+    this.face;
+    this.pairId;
     this.isOpen = false;
     this.isMatched = false;
   }
 
-  logCard() {
+  setFace = (face) => {
+    this.face = face;
+    this.card.children[0].children[1].innerHTML = face;
+  };
+
+  setPair = (_pairId) => {
+    this.pairId = _pairId;
+  };
+
+  setAsRight = () => {
+    this.card.children[0].classList.add('card-right');
+  };
+
+  setAsWrong = () => {
+    this.card.children[0].classList.add('card-wrong');
+  };
+
+  setAsDefault = () => {
+    if (this.card.children[0].classList.contains('card-wrong')) {
+      this.card.children[0].classList.remove('card-wrong');
+    }
+  };
+
+  rotate = () => {
+    if (
+      this.card.children[0].classList.contains('card-rotate') &&
+      !this.card.children[0].classList.contains('card-right')
+    ) {
+      this.card.children[0].classList.remove('card-rotate');
+    } else if (this.card.children[0].classList.contains('card-inner')) {
+      this.card.children[0].classList.add('card-rotate');
+    }
+  };
+
+  getId = () => this.card.id;
+
+  logCard = () => {
     console.log(this.card, this.cardFace);
-  }
+  };
 }
--- source/js/game.js
+++ source/js/game.js
@@ -1,11 +1,19 @@
 class Game {
   constructor(HTMLGameElement) {
     this.game = HTMLGameElement;
+    this.cards = [];
     this.initCards();
     this.flipCard();
+    this.pairs = 6;
+    this.firstOpened = undefined;
+    this.secondOpened = undefined;
   }
 
   initCards = () => {
+    function getRandomArbitrary(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
     const faces = [
       '🐶',
       '🐱',
@@ -29,44 +37,94 @@ class Game {
       '🐊',
       '🐓',
       '🦃',
-      '🐿',
       '🐼',
     ];
 
     const cards = Array.from(document.querySelectorAll('.card'));
     cards.forEach((card, index) => {
-      new Card(card, faces[index]);
+      this.cards.push(new Card(card));
     });
+
+    const tempCards = [...this.cards];
+
+    while (tempCards.length > 0) {
+      const faceIndex = getRandomArbitrary(0, faces.length);
+      const pairIndex = getRandomArbitrary(1, tempCards.length);
+
+      tempCards[0].setFace(faces[faceIndex]);
+      tempCards[pairIndex].setFace(faces[faceIndex]);
+      tempCards[0].setPair(tempCards[pairIndex].getId());
+      tempCards[pairIndex].setPair(tempCards[0].getId());
+      tempCards.splice(pairIndex, 1);
+      tempCards.shift();
+
+      faces.splice(faceIndex, 1);
+    }
   };
 
   flipCard = () => {
     this.game.addEventListener('click', (event) => {
-      if (event.target.parentNode.classList.contains('card-rotate')) {
+      if (
+        event.target.parentNode.parentNode.id === this.firstOpened ||
+        event.target.parentNode.parentNode.id === this.secondOpened
+      ) {
+        return;
+      } else if (
+        event.target.parentNode.classList.contains('card-rotate') &&
+        !event.target.parentNode.classList.contains('card-right') &&
+        event.target.parentNode.parentNode.id !== this.firstOpened &&
+        event.target.parentNode.parentNode.id !== this.secondOpened
+      ) {
         event.target.parentNode.classList.remove('card-rotate');
       } else if (event.target.parentNode.classList.contains('card-inner')) {
         event.target.parentNode.classList.add('card-rotate');
+        if (this.firstOpened === undefined) {
+          this.firstOpened = event.target.parentNode.parentNode.id;
+          console.log(this.firstOpened);
+        } else if (
+          this.firstOpened !== undefined &&
+          this.secondOpened === undefined
+        ) {
+          this.secondOpened = event.target.parentNode.parentNode.id;
+          if (this.compareCards()) {
+            this.cards[this.firstOpened].setAsRight();
+            this.cards[this.secondOpened].setAsRight();
+            this.pairs--;
+            if (this.pairs === 0) {
+              alert('YOU WON!');
+            }
+          } else {
+            this.cards[this.firstOpened].setAsWrong();
+            this.cards[this.secondOpened].setAsWrong();
+          }
+        } else {
+          this.cards[this.firstOpened].rotate();
+          this.cards[this.secondOpened].rotate();
+          this.cards[this.firstOpened].setAsDefault();
+          this.cards[this.secondOpened].setAsDefault();
+
+          this.firstOpened = event.target.parentNode.parentNode.id;
+          this.secondOpened = undefined;
+        }
       }
     });
   };
+
+  compareCards = () => {
+    if (this.cards[this.firstOpened].pairId === this.secondOpened) {
+      console.log(
+        this.cards[this.firstOpened].pairId,
+        this.cards[this.secondOpened]
+      );
+      console.log('YES');
+      return true;
+    } else {
+      console.log(
+        this.cards[this.firstOpened].pairId,
+        this.cards[this.secondOpened]
+      );
+      console.log('NO');
+      return false;
+    }
+  };
 }
-
-// const cards = Array.from(document.querySelectorAll('.card'));
-// const cardFaces = [
-//   '1',
-//   '2',
-//   '3',
-//   '4',
-//   '5',
-//   '6',
-//   '7',
-//   '8',
-//   '9',
-//   '10',
-//   '11',
-//   '12',
-// ];
-
-// cards.forEach((card, index) => {
-//   const temp = new Card(card, cardFaces[index]);
-//   cardRefferencesArray.push(temp);
-// });
